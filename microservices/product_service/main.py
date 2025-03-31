bab03a81ec4405057d5ec0e42c63a2961f272316
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2025-03-30 23:35:28 -0500

    feat: Implement asynchronous product service with MongoDB integration
    - Refactored the product service to use asynchronous gRPC server and methods.
    - Added MongoDB support using Motor for asynchronous database operations.
    - Created a new database configuration file and updated product fetching logic to retrieve products from MongoDB.
    - Introduced a seeding script to populate the database with sample product data.
    - Added Pydantic models for product and category validation.

--- microservices/product_service/main.py
+++ microservices/product_service/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import grpc
+import asyncio
 from concurrent import futures
 from pb import product_pb2_grpc
 from config.grpc import ProductServiceServicer
@@ -8,13 +9,13 @@ from config.grpc import ProductServiceServicer
 load_dotenv()
 grpc_port = os.getenv("GRPC_SERVER_PORT")
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+async def serve():
+    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
     product_pb2_grpc.add_ProductServiceServicer_to_server(ProductServiceServicer(), server)
     server.add_insecure_port(f'[::]:{grpc_port}')
-    server.start()
+    await server.start()
     print(f"ProductService running on port {grpc_port}")
-    server.wait_for_termination()
+    await server.wait_for_termination()
 
 if __name__ == '__main__':
-    serve()
+    asyncio.run(serve())
