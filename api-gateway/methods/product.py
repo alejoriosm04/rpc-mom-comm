7e1517bbb0f1d3f0d63896f25a0541475bacf654
Jhonnathan Ocampo <jsocampod@eafit.edu.co>
2025-03-30 23:45:37 -0500

    imp: queueing messages when ms is not available

--- api-gateway/methods/product.py
+++ api-gateway/methods/product.py
@@ -2,6 +2,7 @@ import logging
 import grpc.aio
 from config.grpc import get_product_stub
 from pb import product_pb2
+from services.rabbitmq_service import publish_to_rabbitmq
 
 logger = logging.getLogger(__name__)
 
@@ -11,23 +12,25 @@ async def get_products_grpc():
         request = product_pb2.ProductRequest()
         response = await stub.GetProducts(request)
         logger.info("Llamada gRPC a GetProducts exitosa.")
+        return _format_products(response.products)
+
     except grpc.aio.AioRpcError as e:
-        logger.error(f"Error en gRPC: {e}")
+        logger.error(f"Error en gRPC. Encolando en RabbitMQ: {e}")
+        # Publicar mensaje de fallo en RabbitMQ para reprocesamiento
+        publish_to_rabbitmq({"action": "get_products", "retry_count": 0})
         return []
 
-    products_list = []
-    for product in response.products:
-        products_list.append({
-            "id": product.id,
-            "title": product.title,
-            "price": product.price,
-            "description": product.description,
-            "category": {
-                "id": product.category.id,
-                "name": product.category.name,
-                "image": product.category.image,
-                "slug": product.category.slug
-            },
-            "images": list(product.images)
-        })
-    return products_list    
+def _format_products(products):
+    return [{
+        "id": product.id,
+        "title": product.title,
+        "price": product.price,
+        "description": product.description,
+        "category": {
+            "id": product.category.id,
+            "name": product.category.name,
+            "image": product.category.image,
+            "slug": product.category.slug
+        },
+        "images": list(product.images)
+    } for product in products] 
