a244424345c5e79bf8d2c1adb118e2e6162f2377
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2025-04-17 21:47:14 -0500

    fix: update CORS settings in api-gateway to allow all origins
    - Changed CORS middleware configuration in app.py to allow requests from any origin by setting allow_origins to "*".

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -20,7 +20,7 @@ app = FastAPI(
 # Middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
